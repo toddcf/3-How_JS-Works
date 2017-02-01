6dc77fb99f566138740e407b14183f5a83883222
Todd Croak-Falen <toddcf@gmail.com>
2017-02-01 15:54:50 -0800

    Console logging 'age' will give different results due to scoping.

--- script.js
+++ script.js
@@ -13,12 +13,17 @@ var retirement =  function(year) {
 retirement(1990);
 
 // Variables
+
 console.log(age);
 var age = 23;
+
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+
+foo();
 console.log(age);
-
-
-
 
 
 
