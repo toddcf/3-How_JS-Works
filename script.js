540d9dc2ac00c3623cc41010b60f9e813f56b192
Todd Croak-Falen <toddcf@gmail.com>
2017-02-01 22:42:53 -0800

    Added function inside the john object, but the 'this' keyword actually points to the window object.

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 calculateAge(1965);
 
 function calculateAge(year) {
-    console.log(2017 - year);
+	console.log(2017 - year);
 }
 
 var retirement =  function(year) {
-    console.log(65 - (2017 - year));
+	console.log(65 - (2017 - year));
 }
 
 retirement(1990);
@@ -20,8 +20,8 @@ console.log(age);
 var age = 23;
 
 function foo() {
-    var age = 65;
-    console.log(age);
+	var age = 65;
+	console.log(age);
 }
 
 foo();
@@ -44,13 +44,13 @@ var a = 'Hello!';
 first();
 
 function first() {
-    var b = 'Hi!';
-    second();
+	var b = 'Hi!';
+	second();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
+	function second() {
+		var c = 'Hey!';
+		console.log(a + b + c);
+	}
 }
 */
 
@@ -63,18 +63,18 @@ var a = 'Hello!';
 first();
 
 function first() {
-    var b = 'Hi!';
-    second();
+	var b = 'Hi!';
+	second();
 
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
+	function second() {
+		var c = 'Hey!';
+		third()
+	}
 }
 
 function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
+	var d = 'John';
+	console.log(a + b + c + d);
 }
 */
 
@@ -88,17 +88,23 @@ function third() {
 calculateAge(1985);
 
 function calculateAge(year) {
-    console.log(2017 - year);
-    console.log(this);
+	console.log(2017 - year);
+	console.log(this);
 }
 */
 
 var john = {
-    name: "John",
-    yearOfBirth: 1990,
-    calculateAge: function() {
-        console.log(this);
-    }
+	name: "John",
+	yearOfBirth: 1990,
+	calculateAge: function() {
+		console.log(this);
+		console.log(2017 - this.yearOfBirth);
+
+		function innerFunction() {
+			console.log(this);
+		}
+		innerFunction();
+	}
 }
 
 john.calculateAge();
